e696f3a28a58a9dfe8aa590949fb2a24c3443216
Christopher Guess <cguess@gmail.com>
2015-10-20 18:17:01 +0200

    forgot to add files on last commit

---
+++ app/src/main/java/com/push/app/AboutActivity.java
@@ -0,0 +1,55 @@
+package com.push.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class AboutActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        initActionBar();
+
+    }
+
+    private void initActionBar() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("About");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if(id==android.R.id.home){
+            onBackPressed();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/push/app/fragment/AboutFragment.java
@@ -0,0 +1,70 @@
+package com.push.app.fragment;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.push.app.R;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.regex.Pattern;
+
+/**
+ * Created by christopher on 10/19/15.
+ */
+
+public class AboutFragment extends Fragment {
+    //TODO: extract this to a central place
+    static final String REGEX = "(?!<a[^>]*?>)(http[^\\s]+)(?![^<]*?</a>)";
+
+
+    public AboutFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_about, container, false);
+        StringBuilder buf=new StringBuilder();
+
+        InputStream htmlStream = null;
+        try {
+
+            htmlStream = getActivity().getAssets().open("about_text.html");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(htmlStream, "UTF-8"));
+            String str;
+
+            while ((str=in.readLine()) != null) {
+                buf.append(str);
+            }
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        TextView textView = (TextView) rootView.findViewById(R.id.about_text);
+        textView.setText(Html.fromHtml(buf.toString()));
+
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+
+        Linkify.addLinks(textView, Pattern.compile(REGEX), "https://");
+        Linkify.addLinks(textView, Pattern.compile(REGEX), "http://");
+
+        return rootView;
+    }
+
+}
