b6ccc1cf194271637609da36969e6e1480c1b658
Christopher Guess <cguess@gmail.com>
2016-04-25 15:23:08 +0200

    updated and moved to dev

--- app/src/main/java/com/push/occrp/util/Contants.java
+++ app/src/main/java/com/push/occrp/util/Contants.java
@@ -9,7 +9,8 @@ public class Contants {
      * URL of your server
      */
 //    public static final String SERVER_URL = "http://chano8.com/wp-json/posts";
-    public static final String SERVER_URL = "http://push-backend.herokuapp.com";
+//    public static final String SERVER_URL = "http://push-backend.herokuapp.com";
+    public static final String SERVER_URL = "https://push-backend-dev.herokuapp.com";
 //    public static final String SERVER_URL = "http://push-137690.nitrousapp.com:3000";
 
 }
